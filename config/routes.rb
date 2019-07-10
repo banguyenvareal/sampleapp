5e35cc51af71a66dd6fe0fa6ea635f03e015d61d
banguyenvareal <nguyen.huu.ba@vareal.vn>
2019-07-10 21:31:51 +0700

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # get "/users/:id", to: "users#show"
   # get '/users', to: 'users#index'
   # post 'users', to: 'users#create'
-  # resources :users
-  root 'static_pages#home'
+  resources :users
+  root 'static_pages#about'
   get  'static_pages/home'
   get  'static_pages/help'
   get  'static_pages/about'
   get  'static_pages/contact'
+  
 end
