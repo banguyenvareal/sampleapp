d4c066fec1f21c89d93e6b00b6d48adbebca32f8
banguyen <nguyen.huu.ba@vareal.vn>
2019-07-12 17:13:56 +0700

    sign up 70%

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+   def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+  def index
+  	@users = User.all
+  	# debugger
+  end
+  def create
+   @user = User.new(user_params)    # Not the final implementation!
+   binding.pry
+    if @user.save
+      # Handle a successful save.
+
+    else
+      render 'new'
+    end
+  end
+  	private
+   def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- app/helpers/dinh_helper.rb
+++
@@ -1,2 +0,0 @@
-module DinhHelper
-end
