bbebc954d983867316a7cfa0c3cf4bce043eb3e9
banguyenvareal <nguyen.huu.ba@vareal.vn>
2019-07-15 00:12:52 +0700

    login 30%

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+    def create
+    	user = User.find_by(email: params[:session][:email].downcase)
+    	if user && user.authenticate(params[:session][:password])
+    			binding.pry
+    	else
+    		render 'new'
+    	end
+    end
+    def destroy
+	end
+ end
+
