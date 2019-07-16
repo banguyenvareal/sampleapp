01d0361a20d23c34fe8fb82e1ec9e493d99f53b3
banguyenvareal <nguyen.huu.ba@vareal.vn>
2019-07-16 22:05:11 +0700

    complete chapter 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_045318) do
+ActiveRecord::Schema.define(version: 2019_07_16_140242) do
 
   create_table "bs", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_045318) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "renember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
