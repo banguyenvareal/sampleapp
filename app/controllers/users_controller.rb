b4133cce41c32b5fc8dda9e8ac988a1e37e57633
banguyen <nguyen.huu.ba@vareal.vn>
2019-07-19 13:37:51 +0700

    chapter 13 ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
-   def show
+  def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
+    @post = @user.microposts.paginate(page: params[:page])
+    # binding.pry
     # debugger
   end
   def index
